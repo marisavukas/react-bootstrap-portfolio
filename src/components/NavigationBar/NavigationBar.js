519eea31462921fe6b7488b5880995eaba10d3c4
Marisa Vukas <mvukas98@gmail.com>
2023-04-28 05:14:56 -0400

    fix navbar links

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,64 +1,23 @@
-import React, { useState } from "react";
-import "./NavigationBar.css";
+import React from "react";
 
-import {
-  Navbar,
-  Container,
-  Nav,
-  Image,
-  spacer,
-  Row,
-  Col,
-  Button,
-  Card,
-} from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 const NavigationBar = () => {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleToggle = () => {
-    if (expanded) {
-      setExpanded(expanded ? !false : "expanded");
-    } else {
-      setExpanded(false);
-    }
-    setExpanded(!expanded);
-  };
   return (
-    <header className="App-header">
-      <Navbar
-        bg="dark"
-        variant="dark"
-        outline="light"
-        expand="lg"
-        expanded={expanded}
-      >
-        <Container className=" d-flex flex-row justify-content-start align-items-start w-100 p-0 px-5 mx-0 h-100">
-          <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            className="p-0 m-0"
-            onClick={handleToggle}
-            isRunning={expanded}
-          />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              <Nav.Link href="#home" onClick={() => setExpanded(false)}>
-                Home
-              </Nav.Link>
-              <Nav.Link href="#about" onClick={() => setExpanded(false)}>
-                About
-              </Nav.Link>
-              <Nav.Link href="#projects" onClick={() => setExpanded(false)}>
-                Projects
-              </Nav.Link>
-              <Nav.Link href="#contact" onClick={() => setExpanded(false)}>
-                Contact
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
+    <Navbar sticky="top">
+      <Container>
+        {" "}
+        <Navbar.Brand>
+          <Nav.Link href="/"> Marisa Vukas</Nav.Link>
+        </Navbar.Brand>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link href="/about-me">About Me</Nav.Link>
+
+          <Nav.Link hrefLang=""="/projects">Projects</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
